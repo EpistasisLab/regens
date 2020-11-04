c3d5d31d7f678f2921d2bbfc911aee87f2c68f51
John Gregg <greggj2016@gmail.com>
2020-11-03 19:15:55 -0500

    setup.py attempt 2

--- setup.py
+++ setup.py
@@ -3,4 +3,10 @@ from setuptools import setup
 setup(
     name = 'regens',
     entry_points = {'console_scripts': ['regens = regens:main']}
+    install_requires = ["bed-reader==0.1.1; python_version > '3.6'",
+                        "numpy==1.16.2",
+                        "pandas==1.1.2",
+                        "matplotlib==3.0.3",
+                        "scipy==1.2.1",
+                        "scikit-learn==0.20.3"]
 )
