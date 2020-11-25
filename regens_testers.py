016349475869189d074cc0cf8da17fd601df6a13
JohnGregg <greggj2016@gmail.com>
2020-11-25 11:35:58 -0500

    black reformatting

--- regens_testers.py
+++ regens_testers.py
@@ -3,15 +3,17 @@ import pandas as pd
 from bed_reader import open_bed
 import pdb
 from matplotlib import pyplot as plt
+
 pd.options.mode.chained_assignment = None
+
 
 def unit_tester(current_output, known_output_file_name, header_type):
 
     """
-    
+
     Purpose
     -------
-    Given input with known output, theis function compare's each function's current output against the correct output. 
+    Given input with known output, theis function compare's each function's current output against the correct output.
 
     Parameters
     ----------
@@ -21,16 +23,33 @@ def unit_tester(current_output, known_output_file_name, header_type):
 
     Returns
     -------
-    It returns nothing. It prints whether or not the function passed it's unit test. 
-    
+    It returns nothing. It prints whether or not the function passed it's unit test.
+
     """
-    if  known_output_file_name[-4:] == ".bed":
-        bed_reader = open_bed("unit_testing_files/correct_write_bed_file_output.bed", count_A1 = True, num_threads = 1)
-        correct_output = bed_reader.read(dtype = 'int8')
+    if known_output_file_name[-4:] == ".bed":
+        bed_reader = open_bed(
+            "unit_testing_files/correct_write_bed_file_output.bed",
+            count_A1=True,
+            num_threads=1,
+        )
+        correct_output = bed_reader.read(dtype="int8")
     elif known_output_file_name[-4:] == ".bim" or known_output_file_name[-4:] == ".fam":
-        correct_output = pd.read_csv("unit_testing_files/" + known_output_file_name, delim_whitespace = True, header = None,  dtype = str).to_numpy().astype("str")
+        correct_output = (
+            pd.read_csv(
+                "unit_testing_files/" + known_output_file_name,
+                delim_whitespace=True,
+                header=None,
+                dtype=str,
+            )
+            .to_numpy()
+            .astype("str")
+        )
     else:
-        correct_output = pd.read_csv("unit_testing_files/" + known_output_file_name, delimiter = "\t", header = header_type).to_numpy()
+        correct_output = pd.read_csv(
+            "unit_testing_files/" + known_output_file_name,
+            delimiter="\t",
+            header=header_type,
+        ).to_numpy()
     function_instance = 0
     if len(correct_output.shape) == 2:
         if (correct_output.shape)[1] == 1:
@@ -47,50 +66,76 @@ def unit_tester(current_output, known_output_file_name, header_type):
     # Importing the values sometimes creates a small ammount of machine error.
     if known_output_file_name[-4:] == ".bim" or known_output_file_name[-4:] == ".fam":
         if np.any(correct_output != current_output):
-            print("The " + function_name + " function's " + known_output_file_name[-4:] + " file output failed its unit test.\n")
+            print(
+                "The "
+                + function_name
+                + " function's "
+                + known_output_file_name[-4:]
+                + " file output failed its unit test.\n"
+            )
         else:
-            print("The " + function_name + " function's " + known_output_file_name[-4:] + " file output passed its unit test.\n")
+            print(
+                "The "
+                + function_name
+                + " function's "
+                + known_output_file_name[-4:]
+                + " file output passed its unit test.\n"
+            )
     else:
         if np.any(np.round(correct_output, 12) != np.round(current_output, 12)):
             if function_instance == "1" or function_instance == "2":
-                print("Instance " + function_instance + " of the " + function_name + " function failed its unit test.\n")
-            else:  
+                print(
+                    "Instance "
+                    + function_instance
+                    + " of the "
+                    + function_name
+                    + " function failed its unit test.\n"
+                )
+            else:
                 print("The " + function_name + " function failed its unit test.\n")
         else:
             if function_instance == "1" or function_instance == "2":
-                print("Instance " + function_instance + " of the " + function_name + " function passed its unit test.\n")
-            else:  
+                print(
+                    "Instance "
+                    + function_instance
+                    + " of the "
+                    + function_name
+                    + " function passed its unit test.\n"
+                )
+            else:
                 print("The " + function_name + " function passed its unit test.\n")
 
 
-def test_drawn_breakpoints(breakpoints, probabilities, chromosome_number, output_plink_filename_prefix):
+def test_drawn_breakpoints(
+    breakpoints, probabilities, chromosome_number, output_plink_filename_prefix
+):
 
     """
-    
+
     Purpose
     -------
-    To show that each breakpoint is, on average, drawn the expected number of times given the "probabilities" 1D array. 
-    To confirms that each row in the "breakpoints" 2D array contains 1 or 0 instances of each breakpoint. 
+    To show that each breakpoint is, on average, drawn the expected number of times given the "probabilities" 1D array.
+    To confirms that each row in the "breakpoints" 2D array contains 1 or 0 instances of each breakpoint.
 
     Parameters
     ----------
-    breakpoints: an NxB numpy array, where N is the number of simulated individuals, and B is the number of breakpoints per chromosome in each individual. 
+    breakpoints: an NxB numpy array, where N is the number of simulated individuals, and B is the number of breakpoints per chromosome in each individual.
                  each breakpoint is an index in probabilities, which directly corresponds to a specific recombination interval.
-    probabilities: a 1D numpy array. The ith element is the probability of drawing a breakpoint at the ith recombination 
-                   interval in a single draw, noting that each row in "breakpoints" cannot contain any breakpoint more than once. 
+    probabilities: a 1D numpy array. The ith element is the probability of drawing a breakpoint at the ith recombination
+                   interval in a single draw, noting that each row in "breakpoints" cannot contain any breakpoint more than once.
     chromosome_number: the cromosome being analyzed
 
     Returns
     -------
-    It returns nothing. It plots the expected number of times each breakpoint index is drawn against the actual number of times. 
-    for each chromosome. It also prints the row numbers of any breakpoint vectors that contain more than one instance of the same breakpoint. 
-    
+    It returns nothing. It plots the expected number of times each breakpoint index is drawn against the actual number of times.
+    for each chromosome. It also prints the row numbers of any breakpoint vectors that contain more than one instance of the same breakpoint.
+
     """
-    
+
     num_breakpoints = len(breakpoints[0])
     num_samples = len(breakpoints)
     breakpoint_indices = list(range(len(probabilities)))
-    breakpoint_counts = dict(zip(breakpoint_indices, len(probabilities)*[0]))
+    breakpoint_counts = dict(zip(breakpoint_indices, len(probabilities) * [0]))
 
     breakpoint_vecs_with_unwanted_replacement = []
     for i, breakpoint_vec in enumerate(breakpoints):
@@ -99,61 +144,83 @@ def test_drawn_breakpoints(breakpoints, probabilities, chromosome_number, output
         for b in breakpoint_vec:
             breakpoint_counts[b] += 1
 
-    print("The samples in the following list have drawn the same breakpoint more than once:")
+    print(
+        "The samples in the following list have drawn the same breakpoint more than once:"
+    )
     print(breakpoint_vecs_with_unwanted_replacement)
     if len(breakpoint_vecs_with_unwanted_replacement) == 0:
         print("Every sample has successfully drawn breakpoints without replacement")
     if len(breakpoint_vecs_with_unwanted_replacement) > 0:
-        print("Some samples have drawn breakpoints with replacement. This is an error, and it should be attended to.")
+        print(
+            "Some samples have drawn breakpoints with replacement. This is an error, and it should be attended to."
+        )
 
     expected_num_breakpoint_counts = []
     num_breakpoint_counts = []
     for index in breakpoint_indices:
-        p_not_drawing_breakpoint = (1 - probabilities[index])**num_breakpoints
+        p_not_drawing_breakpoint = (1 - probabilities[index]) ** num_breakpoints
         p_drawing_breakpoint = 1 - p_not_drawing_breakpoint
-        expected_num_breakpoint_counts.append(num_samples*p_drawing_breakpoint)
+        expected_num_breakpoint_counts.append(num_samples * p_drawing_breakpoint)
         num_breakpoint_counts.append(breakpoint_counts[index])
 
     expected_num_breakpoint_counts = np.array(expected_num_breakpoint_counts)
     num_breakpoint_counts = np.array(num_breakpoint_counts)
 
-    plt.plot(expected_num_breakpoint_counts, num_breakpoint_counts, '*', label = "expected vs actual breakpoint index counts")
-    plt.plot(expected_num_breakpoint_counts, expected_num_breakpoint_counts, '-', label = "y = x")
-    plt.legend(loc = "upper left")
-    plt.xlabel("expected number of breakpoints", fontsize = 14)
-    plt.ylabel("number of breakpoints", fontsize = 14)
+    plt.plot(
+        expected_num_breakpoint_counts,
+        num_breakpoint_counts,
+        "*",
+        label="expected vs actual breakpoint index counts",
+    )
+    plt.plot(
+        expected_num_breakpoint_counts,
+        expected_num_breakpoint_counts,
+        "-",
+        label="y = x",
+    )
+    plt.legend(loc="upper left")
+    plt.xlabel("expected number of breakpoints", fontsize=14)
+    plt.ylabel("number of breakpoints", fontsize=14)
     if "/" in output_plink_filename_prefix:
         prefix = "/".join((output_plink_filename_prefix.split("/"))[:-1]) + "/"
         print(prefix)
     else:
         prefix = ""
-    plt.savefig(prefix + "expected_vs_actual_breakpoint_counts_full_range_chr" + str(chromosome_number) + ".png")
+    plt.savefig(
+        prefix
+        + "expected_vs_actual_breakpoint_counts_full_range_chr"
+        + str(chromosome_number)
+        + ".png"
+    )
     plt.clf()
 
-def test_breakpoint_SNP_mapping(old_breakpoints, rcmb_interval_positions, new_breakpoints, SNP_positions):
+
+def test_breakpoint_SNP_mapping(
+    old_breakpoints, rcmb_interval_positions, new_breakpoints, SNP_positions
+):
 
     """
-    
+
     Purpose
     -------
     To confirm for every drawn breakpoint that, when each breakpoint's recombination interval is replaced with the genomic position of a SNP,
-    that the SNP's genomic position is in-between the genomic positions of the recombination interval's left and right boundaries.     
+    that the SNP's genomic position is in-between the genomic positions of the recombination interval's left and right boundaries.
 
     Parameters
     ----------
-    old_breakpoints: an NxB numpy array, where N is the number of simulated individuals, and B is the number of breakpoints per chromosome in each individual. 
+    old_breakpoints: an NxB numpy array, where N is the number of simulated individuals, and B is the number of breakpoints per chromosome in each individual.
                      each breakpoint is an index in probabilities, which directly corresponds to a specific recombination interval.
-    rcmb_interval_positions: a list where ith index contains the genomic position of the left bound of the recombination interbal, which corresponds 
-                             to the ith element in "probabilities." Note that the left bound of the ith interval is the right bound of the (i-1)th interval. 
-    new_breakpoints: an NxB numpy array, where N is the number of simulated individuals, and B is the number of breakpoints per chromosome in each individual. 
+    rcmb_interval_positions: a list where ith index contains the genomic position of the left bound of the recombination interbal, which corresponds
+                             to the ith element in "probabilities." Note that the left bound of the ith interval is the right bound of the (i-1)th interval.
+    new_breakpoints: an NxB numpy array, where N is the number of simulated individuals, and B is the number of breakpoints per chromosome in each individual.
                      each breakpoint is the row number of a SNP in the input bim file, which directly corresponds to a specific genomic location.
-    SNP_positions: a list where ith index contains the genomic position of the SNP in the input bim file's ith row. 
+    SNP_positions: a list where ith index contains the genomic position of the SNP in the input bim file's ith row.
 
     Returns
     -------
     It returns nothing. It prints a list of the (row, column) coordinates of any new breakpoint with a genomic position that
     does not fall in between the genomic positions of the original recombination interval's left and right boundaries.
-    
+
     """
 
     num_breakpoints = len(old_breakpoints[0])
@@ -163,38 +230,46 @@ def test_breakpoint_SNP_mapping(old_breakpoints, rcmb_interval_positions, new_br
             rcmb_interval_position = rcmb_interval_positions[old_breakpoints[i][k]]
             SNP_position = SNP_positions[new_breakpoints[i][k]]
             next_rcmb_interval = rcmb_interval_positions[old_breakpoints[i][k] + 1]
-            if rcmb_interval_position <= SNP_position and SNP_position <= next_rcmb_interval:
+            if (
+                rcmb_interval_position <= SNP_position
+                and SNP_position <= next_rcmb_interval
+            ):
                 pass
             else:
-                erronious_mappings.append((i,k))
-    print("The following rcmb interval indices have been incorrectly mapped to SNP indices")
+                erronious_mappings.append((i, k))
+    print(
+        "The following rcmb interval indices have been incorrectly mapped to SNP indices"
+    )
     print(erronious_mappings)
     if len(erronious_mappings) == 0:
         print("Every index has been mapped correctly")
     if len(erronious_mappings) > 0:
-        print("Some SNP indices are not contained in the corresponding rcmb interval. This is an error, and it should be attended to.")
-                
+        print(
+            "Some SNP indices are not contained in the corresponding rcmb interval. This is an error, and it should be attended to."
+        )
+
+
 def test_simulated_individuals(breakpoints, sampled_individuals, simulated_individuals):
-    
+
     """
-    
+
     Purpose
     -------
-    To verify that every simulated individual is comprised of the correct segments from the correct real individuals     
+    To verify that every simulated individual is comprised of the correct segments from the correct real individuals
 
     Parameters
     ----------
-    breakpoints: an Nx(B+1) numpy array, where N is the number of simulated individuals, and B is the number of breakpoints per chromosome in each individual. 
+    breakpoints: an Nx(B+1) numpy array, where N is the number of simulated individuals, and B is the number of breakpoints per chromosome in each individual.
                  each breakpoint is the row number of a SNP in the input bim file, which directly corresponds to a specific genomic location. Note that the
-                 last column in breakpoints has all elements equal to the current chromosomes last SNP index. The other columns are simple breakpoint values. 
-    sampled_individuals: an Nx(B+1)xS numpy array, where N is the number of simulated individuals, B is the number of breakpoints per chromosome 
-                         (B breakpoints seperate B+1 segments from B+1 real individuals), and S is the number of SNPs per individual in the current chromosome. 
-    simulated_individuals: an NxS numpy arrray. Each array of S SNPs contains concatenated segments from B+1 individuals.  
+                 last column in breakpoints has all elements equal to the current chromosomes last SNP index. The other columns are simple breakpoint values.
+    sampled_individuals: an Nx(B+1)xS numpy array, where N is the number of simulated individuals, B is the number of breakpoints per chromosome
+                         (B breakpoints seperate B+1 segments from B+1 real individuals), and S is the number of SNPs per individual in the current chromosome.
+    simulated_individuals: an NxS numpy arrray. Each array of S SNPs contains concatenated segments from B+1 individuals.
 
     Returns
     -------
-    It returns nothing. It prints a list of the (row, segment index) of any segments within 
-    simulated individuals that do not map back to the correct region of the correct real individual    
+    It returns nothing. It prints a list of the (row, segment index) of any segments within
+    simulated individuals that do not map back to the correct region of the correct real individual
 
     """
 
@@ -202,20 +277,20 @@ def test_simulated_individuals(breakpoints, sampled_individuals, simulated_indiv
     for j, breakpoint_vec in enumerate(breakpoints):
         startpoint = 0
         for k, breakpoint in enumerate(np.sort(breakpoint_vec)):
-            if np.any(sampled_individuals[j][k][startpoint:breakpoint] != simulated_individuals[j][startpoint:breakpoint]):
+            if np.any(
+                sampled_individuals[j][k][startpoint:breakpoint]
+                != simulated_individuals[j][startpoint:breakpoint]
+            ):
                 incorrect_segment_coordinates.append([j, k])
             startpoint = breakpoint
- 
-    print("\nThe following segments (simulated_individual row number, segment index) are not from the correct regions of the correct real individuals")
+
+    print(
+        "\nThe following segments (simulated_individual row number, segment index) are not from the correct regions of the correct real individuals"
+    )
     print(incorrect_segment_coordinates)
     if len(incorrect_segment_coordinates) == 0:
-        print("Every segment in every simulated individual is from the correct region of the correct real individual")
+        print(
+            "Every segment in every simulated individual is from the correct region of the correct real individual"
+        )
     else:
         print("\nThis is a technical error that needs to be addressed\n")
-
-
-             
-
-        
-        
-        
--- setup.py
+++ setup.py
@@ -1,29 +1,40 @@
 from setuptools import setup
 from setuptools import find_packages
 
-regens_summary = "REGENS (REcombinatory Genome ENumeration of Subpopulations) is an open "
-regens_summary += "source Python package that simulates whole genomes from real genomic "
-regens_summary += "segments. REGENS recombines these segments in a way that simulates "
-regens_summary += "completely new individuals while simultaneously preserving the input "
-regens_summary += "genomes' linkage disequilibrium (LD) pattern with extremely high fedility. "
-regens_summary += "REGENS can also simulate mono-allelic and epistatic single nucleotide variant "
-regens_summary += "(SNV) effects on a continuous or binary phenotype without perturbing the simulated LD pattern."
-regens_summary += "\n\nplease visit our [github page](https://github.com/EpistasisLab/regens) for more information"
+regens_summary = "REGENS (REcombinatory Genome ENumeration "
+regens_summary += "of Subpopulations) is an open source "
+regens_summary += "Python package that simulates whole "
+regens_summary += "genomes from real genomic segments. "
+regens_summary += "REGENS recombines these segments in "
+regens_summary += "a way that simulates completely new "
+regens_summary += "individuals while simultaneously "
+regens_summary += "preserving the input genomes' linkage "
+regens_summary += "disequilibrium (LD) pattern with "
+regens_summary += "extremely high fedility. REGENS can "
+regens_summary += "also simulate mono-allelic and "
+regens_summary += "epistatic single nucleotide variant "
+regens_summary += "(SNV) effects on a continuous or binary "
+regens_summary += "phenotype without perturbing the "
+regens_summary += "simulated LD pattern.\n\nplease visit "
+regens_summary += "our [github page](https://github.com/Ep"
+regens_summary += "istasisLab/regens) for more information."
 
 setup(
-    long_description = regens_summary,
-    long_description_content_type = 'text/markdown',
-    packages = find_packages(),
-    version='0.2.0',
-    python_requires = "==3.7.*",
-    name = 'regens',
-    entry_points = {'console_scripts': ['regens = regens:main']},
-    py_modules = ['regens', 'regens_library', 'regens_testers'],
+    long_description=regens_summary,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    version="0.2.0",
+    python_requires="==3.7.*",
+    name="regens",
+    entry_points={"console_scripts": ["regens = regens:main"]},
+    py_modules=["regens", "regens_library", "regens_testers"],
     setup_requires=["numpy"],
-    install_requires = ["bed-reader==0.1.1",
-                        "numpy==1.16.2",
-                        "pandas==1.1.2",
-                        "matplotlib==3.0.3",
-                        "scipy==1.2.1",
-                        "scikit-learn==0.20.3"]
+    install_requires=[
+        "bed-reader==0.1.1",
+        "numpy==1.16.2",
+        "pandas==1.1.2",
+        "matplotlib==3.0.3",
+        "scipy==1.2.1",
+        "scikit-learn==0.20.3",
+    ],
 )
